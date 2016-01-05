310c210ee4f47e3307ef6c40c2d00ef64a44a868
Laurent GRANIE <lgranie@macepo.local>
2016-01-05 09:45:30 +0100

    v0.3

--- euclidseq.ino
+++ euclidseq.ino
@@ -2,40 +2,36 @@
 #include <SRIO.h>
 #include <LiquidCrystal.h>
 
-  
 //Mode define
 #define MODE_PLAY   0
-#define MODE_BANK   1
-#define MODE_CONFIG 2
+#define MODE_CONFIG_1 1
+
+#define NB_MODE     2
+uint8_t mode = MODE_PLAY;
+
+boolean play_pattern=0;
 
 #define SERIAL_MODE_SERIAL 9600
 #define SERIAL_MODE_MIDI   31250
 #define SERIAL_MODE_USB    115200
+uint32_t serial_mode = SERIAL_MODE_MIDI;
 
-double serial_mode = SERIAL_MODE_MIDI;
+#define NB_BANK 4
+uint8_t bank = 0;
 
-uint8_t mode = MODE_PLAY;
-boolean play_pattern=0;
+#define RYTHM_MODE_POLY 1
+#define RYTHM_MODE_SOLO 2
+uint8_t rythm_mode[NB_BANK][4];
 
-#define BANK_A 0
-#define BANK_B 1
-#define BANK_C 2
-#define BANK_D 3
-
-uint8_t bank = BANK_A;
-
-//Initalize Library
 LiquidCrystal lcd(2,3,4,5,6,7);
 
-char lcd_buffer[16];
 uint8_t lcd_mode = MODE_PLAY;
 
 //-----MIDI-----//            
 uint8_t bpm = 120;//Default BPM
 unsigned int old_clock_time = 0;
 boolean midi_sync = 0; //0 master sync  1 slave sync  Default MasterBPM
-uint8_t midi_channel=1;//Default Midi Channel
-
+uint8_t midi_channel=3;//Default Midi Channel
 
 //Midi message define
 #define MIDI_START 0xfa
@@ -45,24 +41,23 @@ uint8_t midi_channel=1;//Default Midi Channel
 //Time in microsecond of the callback fuction
 uint16_t timer_time = 5000;
 
-uint8_t count_ppqn[4][4] = {-1};//24 pulse par quart note counter
-uint8_t count_step[4][4] = {0};//Step counter
-uint8_t nbr_step[4][4] = {0};//Step counter
-uint8_t nbr_puls[4][4] = {0};
-uint8_t nbr_offs[4][4] = {0};
+uint8_t count_ppqn[NB_BANK][4];
+uint8_t count_step[NB_BANK][4];
+uint8_t track_step[NB_BANK][4];
+uint8_t track_puls[NB_BANK][4];
+int8_t  track_offs[NB_BANK][4];
 
-uint8_t midi_note[4][4] = {{36, 37, 38, 39},{40,41,42,43},{44,45,46,47},{48,49,50,51}};
-uint8_t midi_velo[4][4] = {{127,127,127,127}, {127,127,127,127}, {127,127,127,127}, {127,127,127,127}};
+boolean noteOn[NB_BANK][4];
 
-unsigned long beat_holder[4][4] = {0};
+uint8_t midi_note[NB_BANK][4] = {{36, 37, 38, 39}, {40,41,42,43}, {44,45,46,47}, {48,49,50,51}};
+uint8_t midi_velo[NB_BANK][4];
 
-uint8_t scale[4][4]={1};//Default scale  1/16
-uint8_t scale_value[]={
-  3, 6, 8, 12,
-  16, 24};
+uint32_t beat_holder[NB_BANK][4];
+
+uint8_t scale[NB_BANK][4]={16};//Default scale  1/16
 
 // Licklogic
-uint8_t valuePot[4][4][4]    = {0}; //  valeur des potentiomètres stockées dans un array / bank
+uint8_t valuePot[NB_BANK][4][4] = {0}; 
 
 // Buttons et LED
 #define BUTTON_RED         7
@@ -92,7 +87,7 @@ void setup() {
 
   lcd.begin(16,2);
   lcd.clear();
-  lcd.print("   EUCLIDE SEQ  ");
+  lcd.print("  EUCLIDE SEQ   ");
   
   Serial.begin(serial_mode);
   
@@ -115,8 +110,24 @@ void setup() {
   
   // Init Buttons
   SR.Initialize();
+  delay(100);
+  
   SR.Led_All_On();
-  delay(1000);
+  // Init default value
+  for(int i = 0; i < 4; i++) {
+    for(int j = 1; j < 4; j++) {
+      rythm_mode[i][j] = RYTHM_MODE_POLY;
+      count_ppqn[i][j] = -1;
+      count_step[i][j] = 0;
+      track_step  [i][j] = 0;
+      track_puls  [i][j] = 0;
+      track_offs  [i][j] = 0;
+      midi_velo [i][j] = 127;
+      scale     [i][j] = 16;
+      noteOn    [i][j] = false;
+    }
+  }
+  delay(100);
   SR.Led_All_Off();
 }
 //--------------------------------------------SETUP----------------------------------------------//
@@ -156,20 +167,26 @@ void checkPotValues() {
               midi_note[bank][c] = current_pot_value >> 1; // 0->128
               break;
             case 1 :
-              nbr_step[bank][c] = current_pot_value * 32 / 255;
-              nbr_puls[bank][c] = valuePot[bank][c][2] * nbr_step[bank][c] / 255;
-              nbr_offs[bank][c] = valuePot[bank][c][3] * nbr_puls[bank][c] / 255;
+              track_step[bank][c] = current_pot_value * 32 / 255;
+              
+              if(rythm_mode[bank][c] == RYTHM_MODE_SOLO)
+                scale[bank][c] = (96 / track_step[bank][c]) * 2; // 96 = 24 ppqn x 4 noires = nb ppqm / bar
+              else
+                scale[bank][c] = 6;
+              
+              track_puls[bank][c] = valuePot[bank][c][2] * track_step[bank][c] / 255;
+              track_offs[bank][c] = valuePot[bank][c][3] * track_puls[bank][c] / 255;
               break;
             case 2 :
-              nbr_puls[bank][c] = current_pot_value * nbr_step[bank][c] / 255;
-              nbr_offs[bank][c] = valuePot[bank][c][3] * nbr_puls[bank][c] / 255;
+              track_puls[bank][c] = current_pot_value * track_step[bank][c] / 255;
+              track_offs[bank][c] = valuePot[bank][c][3] * track_puls[bank][c] / 255;
               break;
             case 3 :
-              nbr_offs[bank][c] = current_pot_value * nbr_step[bank][c] / 255; 
+              track_offs[bank][c] = current_pot_value * track_step[bank][c] / 255 - track_step[bank][c] / 2; 
               break;
           }
           
-          beat_holder[bank][c] = euclid(nbr_step[bank][c], nbr_puls[bank][c]);
+          beat_holder[bank][c] = euclid(track_step[bank][c], track_puls[bank][c], track_offs[bank][c]);
           
           lcd.clear();
           lcd.setCursor(0,0);
@@ -177,11 +194,11 @@ void checkPotValues() {
           lcd.setCursor(0,1);
           lcd.print(midi_note[bank][c],DEC);
           lcd.setCursor(4,1);
-          lcd.print(nbr_step[bank][c],DEC);
+          lcd.print(track_step[bank][c],DEC);
           lcd.setCursor(8,1);
-          lcd.print(nbr_puls[bank][c],DEC);
+          lcd.print(track_puls[bank][c],DEC);
           lcd.setCursor(12,1);
-          lcd.print(beat_holder[bank][c],DEC);
+          lcd.print((int) track_offs[bank][c],DEC);
         }
       }
     }
@@ -189,40 +206,18 @@ void checkPotValues() {
 }
 
 void checkButValues() {
-  if(mode == MODE_PLAY) {
-    if(SR.Button_Pin_Read(BUTTON_RED)) {
-      play_pattern = !play_pattern;
-      if(play_pattern) {
-        Serial.write(MIDI_START);
-      } else {
-        Serial.write(MIDI_STOP);
-      }
-       
-      lcd.clear();
-      delay(200);
+  if(SR.Button_Pin_Read(BUTTON_RED)) {
+    play_pattern = !play_pattern;
+    if(play_pattern) {
+      Serial.write(MIDI_START);
+    } else {
+      Serial.write(MIDI_STOP);
     }
+    delay(200);
+    return;
   }
-    
-  if(mode == MODE_BANK) {
-    if(SR.Button_Pin_Read(BUTTON_BLACK_01)) {
-      bank = BANK_A;
-      delay(200);
-    }
-    if(SR.Button_Pin_Read(BUTTON_BLACK_02)) {
-      bank = BANK_B;
-      delay(200);
-    }
-    if(SR.Button_Pin_Read(BUTTON_BLACK_03)) {
-      bank = BANK_C;
-      delay(200);
-    }
-    if(SR.Button_Pin_Read(BUTTON_BLACK_04)) {
-      bank = BANK_D;
-      delay(200);
-    }
-  }
-  
-  if(mode == MODE_CONFIG) {
+ 
+  if(mode == MODE_CONFIG_1) {
     if(SR.Button_Pin_Read(BUTTON_BLACK_01)) {
       midi_sync = !midi_sync;
       delay(200);
@@ -242,6 +237,21 @@ void checkButValues() {
       delay(200);
     }
     
+    if(SR.Button_Pin_Read(BUTTON_BLACK_03)) {
+      if(SR.Button_Pin_Read(BUTTON_WHITE_LEFT)) {
+        bank--;
+        bank = (bank + NB_BANK) % NB_BANK;
+        delay(200);
+        return;
+      }
+      if(SR.Button_Pin_Read(BUTTON_WHITE_RIGHT)) {
+        bank++;
+        bank = bank % NB_BANK;
+        delay(200);
+        return;
+      }
+    }
+    
     if(SR.Button_Pin_Read(BUTTON_BLACK_04)) {
       if(SR.Button_Pin_Read(BUTTON_WHITE_LEFT)) {
         if(!midi_sync) {
@@ -258,58 +268,31 @@ void checkButValues() {
         return;
       }
     }
+    
   }
   
   // Navigation
   if(SR.Button_Pin_Read(BUTTON_WHITE_LEFT)) {
-    if(mode == 0)
-      mode = 2;
-    else
-      mode = (mode - 1) % 3;
+    mode--;
+    mode = (mode + NB_MODE) % NB_MODE;
     delay(200);
-  }  
+    return;
+  }
+  
   if(SR.Button_Pin_Read(BUTTON_WHITE_RIGHT)) {
-    mode = (mode + 1) % 3;
+    mode++;
+    mode = mode % NB_MODE;
     delay(200);
+    return;
   }
 
 }
 
-void updateLeds() {
-  if(mode == MODE_PLAY) {
-    if(play_pattern) {
-      SR.Led_Pin_Write(LED_RED, 1);
-    } else {
-      SR.Led_Pin_Write(LED_RED, 0); 
-    }
-    
-    for(int inst = 0; inst < 4; inst++) {
-      if (bitRead (beat_holder[bank][inst], count_step[bank][inst])) {   
-        SR.Led_Pin_Write(LED_INST[inst], 1);
-      } else {
-        SR.Led_Pin_Write(LED_INST[inst], 0);
-      }
-    }
-  }
-  
-  if(mode == MODE_BANK) {
-    SR.Led_All_Off();
-    if(bank == BANK_A) {
-      SR.Led_Pin_Write(LED_BLACK_01, 1);
-    }
-    if(bank == BANK_B) {
-      SR.Led_Pin_Write(LED_BLACK_02, 1);
-    }
-    if(bank == BANK_C) {
-      SR.Led_Pin_Write(LED_BLACK_03, 1);
-    }
-    if(bank == BANK_D) {
-      SR.Led_Pin_Write(LED_BLACK_04, 1);
-    }
-  }
-  
-  if(mode == MODE_CONFIG) {
-    SR.Led_All_Off();
+void updateLeds() {  
+  if(play_pattern) {
+    SR.Led_Pin_Write(LED_RED, 1);
+  } else {
+     SR.Led_Pin_Write(LED_RED, 0); 
   }
 }
 
@@ -330,42 +313,29 @@ void updateLcd() {
     } 
   }
   
-  if(mode == MODE_BANK) {
+  if(mode == MODE_CONFIG_1) {
     if(lcd_mode != mode) {
-      lcd_mode = MODE_BANK;
+      lcd_mode = MODE_CONFIG_1;
       lcd.clear();
     }
     lcd.setCursor(0,0);
-    lcd.print("BANK");
-    lcd.setCursor(0,1);
-    lcd.print(bank);
-  }
-  
-  if(mode == MODE_CONFIG) {
-    if(lcd_mode != mode) {
-      lcd_mode = MODE_CONFIG;
-      lcd.clear();
-    }
-    lcd.setCursor(0,0);
-    lcd.print("SYNC");
+    lcd.print("SYC BUS BNK BPM ");
     lcd.setCursor(0,1);
     if(midi_sync)
       lcd.print("YES ");
     else
       lcd.print("NO  ");
-      
-    lcd.setCursor(4,0);
-    lcd.print("SER");
     lcd.setCursor(4,1);
     if(serial_mode == SERIAL_MODE_USB)
       lcd.print("USB ");
     else if(serial_mode == SERIAL_MODE_MIDI)
-      lcd.print("MID");
+      lcd.print("MID ");
     else
-      lcd.print("SER");
-      
-    lcd.setCursor(12,0);
-    lcd.print("BPM");
+      lcd.print("SER ");
+    
+    lcd.setCursor(8,1);
+    lcd.print(bank, DEC);
+    
     lcd.setCursor(12,1);
     lcd.print(bpm, DEC);
   }
@@ -373,16 +343,23 @@ void updateLcd() {
 }
   
 void updateMidi() {
-  //
-  
   for(int inst = 0; inst < 4; inst++) {
-    if (bitRead (beat_holder[bank][inst], count_step[bank][inst])) {  
-      Send_NoteON(midi_note[bank][inst], midi_velo[bank][inst]);
+    if (bitRead (beat_holder[bank][inst], count_step[bank][inst])) {
+      if(!noteOn[bank][inst]) {
+        Send_NoteON(midi_note[bank][inst], midi_velo[bank][inst]);  
+        SR.Led_Pin_Write(LED_INST[inst], 1);
+        noteOn[bank][inst] = true;
+      } else {
+        //Send_NoteCONT(midi_note[bank][inst]);
+      }
     } else {
-      Send_NoteOFF(midi_note[bank][inst]);
+      if(noteOn[bank][inst]) {
+        Send_NoteOFF(midi_note[bank][inst]);  
+        SR.Led_Pin_Write(LED_INST[inst], 0);
+        noteOn[bank][inst] = false;
+      }
     }
   }
-  
 }
 
 //////////////////////////////////////////////////////////////////
@@ -396,7 +373,7 @@ void Count_PPQN() {
       byte data = Serial.read();
       if(data == MIDI_START) {
           play_pattern = 1;
-          for(int i = 0; i < 4; i++) {
+          for(int i = 0; i < NB_BANK; i++) {
             for(int j = 0; j < 4; j++) {
               count_ppqn[i][j] = -1;
             }
@@ -405,25 +382,22 @@ void Count_PPQN() {
       }
       else if(data == MIDI_STOP ) {
         play_pattern = 0;
-        for(int i = 0; i < 4; i++) {
+        for(int i = 0; i < NB_BANK; i++) {
           for(int j = 0; j < 4; j++) {
             count_step[i][j] = 0;
             count_ppqn[i][j] = -1;
           }
         }
       }
-      else if(data == MIDI_CLOCK && (play_pattern == 1 )) {
+      else if(data == MIDI_CLOCK && play_pattern) {
         int time = millis();
-        lcd.clear();
-        if(time > old_clock_time) {
-          bpm = (1000 / (time - old_clock_time) / 24) * 60;
-          old_clock_time = time;
-        }
-        for(int i = 0; i < 4; i++) {
+        bpm = (time - old_clock_time) / 24 * 60000;
+        old_clock_time = time;
+        for(int i = 0; i < NB_BANK; i++) {
           for(int j = 0; j < 4; j++) {
-            count_ppqn[i][j]++;
-            count_step[i][j] = count_ppqn[i][j] / scale_value[scale[i][j]];
-            if(count_ppqn[i][j] >= (nbr_step[i][j] * scale_value[scale[i][j]]) -1){
+            count_ppqn[i][j]++;    
+            count_step[i][j] = count_ppqn[i][j] / scale[i][j];   
+            if(count_ppqn[i][j] >= (track_step[i][j] * scale[i][j]) - 1) {
               count_ppqn[i][j] = -1;
             }
           }
@@ -436,149 +410,83 @@ void Count_PPQN() {
   if(!midi_sync){
     timer_time=2500000/bpm;
     Serial.write(MIDI_CLOCK);
-    if(play_pattern) {   
-      for(int i = 0; i < 4; i++) {
-        for(int j = 0; j < 4; j++) {
-          count_ppqn[i][j]++;    
-          count_step[i][j] = count_ppqn[i][j] / scale_value[scale[i][j]];   
-          if(count_ppqn[i][j] >= (nbr_step[i][j] * scale_value[scale[i][j]])-1){
-            count_ppqn[i][j] = -1;
-          }
-        }
-      }
-    }
-    else if(!play_pattern) {
-      for(int i = 0; i < 4; i++) {
+    if(!play_pattern) {
+      for(int i = 0; i < NB_BANK; i++) {
         for(int j = 0; j < 4; j++) {
           count_ppqn[i][j] = -1;
           count_step[i][j] = 0;
         }
       }
+    } else {   
+      for(int i = 0; i < NB_BANK; i++) {
+        for(int j = 0; j < 4; j++) {
+          count_ppqn[i][j]++;    
+          count_step[i][j] = count_ppqn[i][j] / scale[i][j];   
+          if(count_ppqn[i][j] >= (track_step[i][j] * scale[i][j]) - 1) {
+            count_ppqn[i][j] = -1;
+          }
+        }
+      }
     }
   }
 }
 
-// Euclid calculation function 
-byte[] euclid(int steps, int pulses){ // inputs: n=total, k=beats, o = offset
-  if(pulses == 0) {
-    return 0;
-  }
+// Euclid calculation function
+uint32_t bits;
+uint8_t pos;
+int8_t remainder[32];
+int8_t count[32];
   
-  byte workbeat[steps];
-  
-  int a; 
-  for (a = 0; a < steps; a++) { // Populate workbeat with unsorted pulses and pauses 
-    if (a<pulses)
-      workbeat[a] = 1;
-    else 
-      workbeat[a] = 0;
-  }
-  
-  int pauses = steps - pulses;
-  int per_pulse = pauses / pulses;
-  int remainder = pauses % pulses;  
-  unsigned int outbeat;
-  unsigned int working;
-  int workbeat_count=steps;
-  int b; 
-  int trim_count;
-
-  if (per_pulse>0 && remainder < 2) { // Handle easy cases where there is no or only one remainer  
-    for (a=0;a<pulses;a++){
-      for (b=workbeat_count-1; b>workbeat_count-per_pulse-1;b--){
-        workbeat[a]  = ConcatBin (workbeat[a], workbeat[b]);
-      }
-      workbeat_count = workbeat_count-per_pulse;
-    }
-
-    outbeat = 0; // Concatenate workbeat into outbeat - according to workbeat_count 
-    for (a=0;a < workbeat_count;a++){
-      outbeat = ConcatBin(outbeat,workbeat[a]);
-    }
-    return outbeat;
-  }
-
-  else { 
-
-    int groupa = pulses;
-    int groupb = pauses; 
+uint32_t euclid(int8_t slots, int8_t pulses, int8_t offset) {
+    bits = 0UL;
+    pos = 0;
+    if(!pulses)
+      return bits;
     
-    while(groupb>1){ //main recursive loop
-      if (groupa>groupb){ // more Group A than Group B
-        int a_remainder = groupa-groupb; // what will be left of groupa once groupB is interleaved 
-        trim_count = 0;
-        for (a=0; a<groupa-a_remainder;a++){ //count through the matching sets of A, ignoring remaindered
-          workbeat[a]  = ConcatBin (workbeat[a], workbeat[workbeat_count-1-a]);
-          trim_count++;
-        }
-        workbeat_count = workbeat_count-trim_count;
-
-        groupa=groupb;
-        groupb=a_remainder;
-      }
-
-      else if (groupb>groupa){ // More Group B than Group A
-        int b_remainder = groupb-groupa; // what will be left of group once group A is interleaved 
-        trim_count=0;
-        for (a = workbeat_count-1;a>=groupa+b_remainder;a--){ //count from right back through the Bs
-          workbeat[workbeat_count-a-1] = ConcatBin (workbeat[workbeat_count-a-1], workbeat[a]);
-
-          trim_count++;
-        }
-        workbeat_count = workbeat_count-trim_count;
-        groupb=b_remainder;
-      }
-      
-      else if (groupa == groupb){ // groupa = groupb 
-        trim_count=0;
-        for (a=0;a<groupa;a++){
-          workbeat[a] = ConcatBin (workbeat[a],workbeat[workbeat_count-1-a]);
-          trim_count++;
-        }
-        workbeat_count = workbeat_count-trim_count;
-        groupb=0;
-      }
-
-      else {
-        //        Serial.println("ERROR");
-      }
+    int8_t divisor = slots - pulses;
+    remainder[0] = pulses; 
+    int8_t level = 0; 
+    int8_t cycleLength = 1; 
+    int8_t remLength = 1;
+    do { 
+      count[level] = divisor / remainder[level]; 
+      remainder[level+1] = divisor % remainder[level]; 
+      divisor = remainder[level]; 
+      int8_t newLength = (cycleLength * count[level]) + remLength; 
+      remLength = cycleLength; 
+      cycleLength = newLength; 
+      level = level + 1;
+    }while(remainder[level] > 1);
+    
+    count[level] = divisor; 
+    if(remainder[level] > 0)
+      cycleLength = (cycleLength * count[level]) + remLength;
+    build(level);
+    
+    // Apply offset
+    if(offset < 0) {
+       return (bits << offset) | (bits >> (sizeof(bits) + offset));
+    } else if(offset > 0) {
+       return (bits >> offset) | (bits << (sizeof(bits) - offset));
     }
-
-
-    outbeat = 0; // Concatenate workbeat into outbeat - according to workbeat_count 
-    for (a=0;a < workbeat_count;a++){
-      outbeat = ConcatBin(outbeat,workbeat[a]);
-    }
-    return outbeat;
-
+    
+    return bits;
   }
-}
 
-// Function to find the binary length of a number by counting bitwise 
-int findlength(unsigned int bnry){
-  boolean lengthfound = false;
-  int length=1; // no number can have a length of zero - single 0 has a length of one, but no 1s for the sytem to count
-  for (int q=32;q>=0;q--){
-    int r=bitRead(bnry,q);
-    if(r==1 && lengthfound == false){
-      length=q+1;
-      lengthfound = true;
+  void build(int8_t level){
+    if(level == -1){
+      bits &= ~(1UL<<pos++);
+    }else if(level == -2){
+      bits |= 1UL<<pos++;
+    }else{ 
+      for(int8_t i=0; i < count[level]; i++)
+	build(level-1); 
+      if(remainder[level] != 0)
+	build(level-2); 
     }
   }
-  return length;
-}
 
-// Function to concatenate two binary numbers bitwise 
-unsigned int ConcatBin(unsigned int bina, unsigned int binb){
-  int binb_len=findlength(binb);
-  unsigned int sum=(bina<<binb_len);
-  sum = sum | binb;
-  return sum;
-}
-
-
-
-void Send_NoteON(byte data, uint8_t velo){
+void Send_NoteON(byte data, uint8_t velo) {
   Serial.write (144+midi_channel-1);//Note ON on selected channel 
   Serial.write (data);
   Serial.write (velo);
