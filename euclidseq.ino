e7190b14d5b5bffdce1373efc2d4b660ac1263f1
Laurent GRANIE <lgranie@macepo.local>
2016-01-05 09:50:45 +0100

    v0.5

--- euclidseq.ino
+++ euclidseq.ino
@@ -1,3 +1,8 @@
+/*
+* Author:       Laurent Granié
+* Copyright:    (C) 2014 Laurent Granié
+* Licence:      GNU General Public Licence version 3
+*/
 #include <TimerOne.h>
 #include <SRIO.h>
 #include <LiquidCrystal.h>
@@ -21,8 +26,8 @@ uint32_t serial_mode = SERIAL_MODE_MIDI;
 uint8_t bank = 0;
 uint8_t inst = 0;
 
-#define RYTHM_MODE_POLY 1
-#define RYTHM_MODE_SOLO 2
+#define RYTHM_MODE_POLY 0
+#define RYTHM_MODE_SOLO 1
 uint8_t rythm_mode[NB_BANK][4];
 
 LiquidCrystal lcd(2,3,4,5,6,7);
@@ -38,6 +43,7 @@ uint8_t lcd_mode_play = LCD_MODE_PLAY_00;
 uint8_t old_lcd_mode_play = LCD_MODE_PLAY_00;
 
 boolean bitmap_mode = false;
+uint8_t bitmap_mode_step = 0;
 boolean pot_change  = true;
 
 //-----MIDI-----//            
@@ -50,6 +56,8 @@ uint8_t midi_channel=1;//Default Midi Channel
 #define MIDI_START 0xfa
 #define MIDI_STOP  0xfc
 #define MIDI_CLOCK 0xf8
+const char* midi_note_labels[] = {"C%d", "C%d#", "D%d", "D%d#", "E%d", "F%d", "F%d#", "G%d", "G%d#", "A%d", "A%d#", "B%d"};
+char midi_note_label[3];
 
 //Time in microsecond of the callback fuction
 uint16_t timer_time = 5000;
@@ -60,7 +68,7 @@ uint8_t track_step[NB_BANK][4];
 uint8_t track_last_step[NB_BANK][4];
 uint8_t track_puls[NB_BANK][4];
 int8_t  track_offs[NB_BANK][4];
-float   track_swing[NB_BANK][4];
+uint8_t track_swing[NB_BANK][4];
 
 boolean noteOn[NB_BANK][4];
 
@@ -95,6 +103,8 @@ uint8_t valuePot[NB_BANK][4][4];
 
 uint8_t LED_INST[4] = {LED_BLACK_01, LED_BLACK_02, LED_BLACK_03, LED_BLACK_04};
 
+int i, j;
+
 //--------------------------------------------SETUP----------------------------------------------//
 void setup() {
   // Attach callback function to Timer1
@@ -125,15 +135,13 @@ void setup() {
   
   // Init Buttons
   SR.Initialize();
-  delay(100);
-  
   SR.Led_All_On();
   // Init default value
   beat_holder = (boolean***) malloc(NB_BANK * sizeof * beat_holder);
-  for(int i = 0; i < 4; i++) {
+  for(i = 0; i < 4; i++) {
     beat_holder[i] = (boolean**) malloc(4 * sizeof * beat_holder[i]);
     
-    for(int j = 0; j < 4; j++) {
+    for(j = 0; j < 4; j++) {
       rythm_mode [i][j] = RYTHM_MODE_POLY;
       count_ppqn [i][j] = -1;
       count_step [i][j] = 0;
@@ -141,7 +149,7 @@ void setup() {
       track_puls [i][j] = 0;
       track_offs [i][j] = 0;
       track_scale[i][j] = 6;
-      track_swing[i][j] = 1.0f;
+      track_swing[i][j] = 0;
       track_last_step[i][j] = 0;
       midi_velo  [i][j] = 100;
       noteOn     [i][j] = false;
@@ -173,16 +181,15 @@ void loop() {
 }
 
 void checkPotValues() {
-  float valuef, d;
   int cols[4] = {1,0,3,2};
+  
   uint8_t current_pot_value, diff;
   for (int ci=0; ci < 4; ci++) {
     int c = cols[ci];
-    int value = 0;
     for (int l=0; l< 4; l++) {
       digitalWrite(8,LOW);
-      uint32_t data = (analogRead(c / 2) >> 2);//convert 10bits 0->1024 to 8bits 0->255
-      current_pot_value = map(data,6,253,0,255);// map to right midi value 0,127 due to the bad ADC and bad power stability
+      uint32_t data = analogRead(c / 2) >> 2;//convert 10bits 0->1024 to 8bits 0->255
+      current_pot_value = map(data,6,247,0,255);// map to right value 0,127 due to the bad ADC and bad power stability
       current_pot_value = max(current_pot_value,0);  
       current_pot_value = constrain(current_pot_value,0,255);
       digitalWrite(8,HIGH);
@@ -195,88 +202,97 @@ void checkPotValues() {
             case 0 :
               break;
             case 1 :
-              current_pot_value = current_pot_value >> 1; // 0->128
+              current_pot_value = current_pot_value * 127 / 255;
               diff = abs(current_pot_value - midi_note[bank][c]);
-              if(diff > 0 && diff < 2) {
+              if(diff == 1) {
                 inst = c;
                 midi_note[bank][inst] = current_pot_value;
-                pot_change = true;          
+                pot_change = true;
               }
               break;
             case 2 :
-              valuef = ((float) current_pot_value / 255) + 1.f;  // 1 <= x <= 2
-              d = abs(valuef - track_swing[bank][c]);
-              if(d > 0.0f && d < 0.02f) {
+              current_pot_value = current_pot_value * track_scale[bank][c] / 255;
+              diff = abs(current_pot_value - track_swing[bank][c]);
+              if(diff == 1) {
                 inst = c;
-                track_swing[bank][inst] = valuef;
+                track_swing[bank][inst] = current_pot_value;
                 pot_change = true;
               }
               break;
             case 3 :
-              current_pot_value = (current_pot_value / 128) + 1; // 0->1
+              current_pot_value = current_pot_value / 255; // 0->1
               if (rythm_mode[bank][c] != current_pot_value) {
                 inst = c;
-                rythm_mode[bank][c] = current_pot_value;
+                rythm_mode[bank][inst] = current_pot_value;
+                
+                // Update scale
+                if(rythm_mode[bank][inst] == RYTHM_MODE_SOLO && track_step[bank][inst] != 0) {
+                  track_scale[bank][inst] = (96 / track_step[bank][inst]) * 2; // 96 = 24 ppqn x 4 noires = nb ppqm / bar
+                } else {
+                  track_scale[bank][inst] = 6;
+                }
                 pot_change = true;
               }
               break;
-          }
-        } else {
-          lcd_mode_play = LCD_MODE_PLAY_00;
-          diff = abs(current_pot_value - valuePot[bank][c][l]);
-          if (diff > 0 && diff < 16) {
-            inst = c;
-            valuePot[bank][inst][l] = current_pot_value;
-            
-            switch(l) {
-              case 0 :
-                continue;
-              case 1 :
-                value = current_pot_value * 32 / 255;
-                if(track_step[bank][inst] == value) {
-                  continue;
-                } else {
-                  track_step[bank][inst] = value;
-                  pot_change = true;
-                }
-                
-                if(rythm_mode[bank][inst] == RYTHM_MODE_SOLO && track_step[bank][inst] != 0) {
-                  track_scale[bank][inst] = (96 / track_step[bank][inst]) * 2; // 96 = 24 ppqn x 4 noires = nb ppqm / bar
-                } else
-                  track_scale[bank][inst] = 6;
-                
-                track_puls[bank][inst] = valuePot[bank][inst][2] * track_step[bank][inst] / 255;
-                track_offs[bank][inst] = valuePot[bank][inst][3] * track_step[bank][inst] / 255;
-                break;
-              case 2 :
-                value = current_pot_value * track_step[bank][inst] / 255;
-                if(track_puls[bank][inst] == value) {
-                  continue;
-                } else {
-                  track_puls[bank][inst] = value;
-                  pot_change = true;
-                }
-              
-                track_offs[bank][inst] = valuePot[bank][inst][3] * track_step[bank][inst] / 255;
-                break;
-              case 3 :
-                value = current_pot_value * track_step[bank][inst] / 255;
-                if(track_offs[bank][inst] == value) {
-                  continue;
-                } else {
-                  track_offs[bank][inst] = value;
-                  pot_change = true;
-                }
-                
-                track_offs[bank][inst] = current_pot_value * track_step[bank][inst] / 255; 
-                break;
             }
-            
+            continue;
+          } else {
+            lcd_mode_play = LCD_MODE_PLAY_00;
+            diff = abs(current_pot_value - valuePot[bank][c][l]);
+            if (diff > 0 && diff < 8) {
+              inst = c;
+              valuePot[bank][inst][l] = current_pot_value;
+              
+              switch(l) {
+                case 0 :
+                  break;
+                case 1 :
+                  current_pot_value = current_pot_value * 32 / 255;
+                  if(track_step[bank][inst] == current_pot_value) {
+                    break;
+                  } else {
+                    track_step[bank][inst] = current_pot_value;
+                    pot_change = true;
+                  }
+                  
+                  if(rythm_mode[bank][inst] == RYTHM_MODE_SOLO && track_step[bank][inst] != 0) {
+                    track_scale[bank][inst] = (96 / track_step[bank][inst]) * 2; // 96 = 24 ppqn x 4 noires = nb ppqm / bar
+                  } else {
+                    track_scale[bank][inst] = 6;
+                  }
+                  
+                  track_puls[bank][inst] = valuePot[bank][inst][2] * track_step[bank][inst] / 255;
+                  track_offs[bank][inst] = valuePot[bank][inst][3] * track_step[bank][inst] / 255;
+                  break;
+                case 2 :
+                  current_pot_value = current_pot_value * track_step[bank][inst] / 255;
+                  if(track_puls[bank][inst] == current_pot_value) {
+                    break;
+                  } else {
+                    track_puls[bank][inst] = current_pot_value;
+                    pot_change = true;
+                  }
+                
+                  track_offs[bank][inst] = valuePot[bank][inst][3] * track_step[bank][inst] / 255;
+                  break;
+                case 3 :
+                  current_pot_value = current_pot_value * track_step[bank][inst] / 255;
+                  if(track_offs[bank][inst] == current_pot_value) {
+                    break;
+                  } else {
+                    track_offs[bank][inst] = current_pot_value;
+                    pot_change = true;
+                  }
+                  
+                  track_offs[bank][inst] = current_pot_value * track_step[bank][inst] / 255; 
+                  break;
+              }
             euclid(bank, inst);
           }
+          continue;
         }
       }
-    }
+    } 
   }
 }
 
@@ -287,8 +303,9 @@ void checkButValues() {
       Serial.write(MIDI_START);
     } else {
       Serial.write(MIDI_STOP);
+      bitmap_mode_step = 0;
     }
-    delay(200);
+    delay(100);
     return;
   }
  
@@ -296,14 +313,14 @@ void checkButValues() {
     if(SR.Button_Pin_Read(BUTTON_BLACK)) {
       bitmap_mode = !bitmap_mode;
       pot_change = true;
-      delay(200);
+      delay(100);
     }
   }
  
   if(mode == MODE_CONFIG_1) {
     if(SR.Button_Pin_Read(BUTTON_BLACK_01)) {
       midi_sync = !midi_sync;
-      delay(200);
+      delay(100);
       return;
     }
     if(SR.Button_Pin_Read(BUTTON_BLACK_02)) {
@@ -315,7 +332,7 @@ void checkButValues() {
         serial_mode = SERIAL_MODE_USB;
       
       Serial.begin(serial_mode);
-      delay(200);
+      delay(100);
       return;
       
     }
@@ -323,13 +340,13 @@ void checkButValues() {
       if(SR.Button_Pin_Read(BUTTON_WHITE_LEFT)) {
         bank--;
         bank = (bank + NB_BANK) % NB_BANK;
-        delay(200);
+        delay(100);
         return;
       }
       if(SR.Button_Pin_Read(BUTTON_WHITE_RIGHT)) {
         bank++;
         bank = bank % NB_BANK;
-        delay(200);
+        delay(100);
         return;
       }
     }
@@ -342,7 +359,7 @@ void checkButValues() {
       if(SR.Button_Pin_Read(BUTTON_WHITE_RIGHT)) {
         midi_channel++;
       }
-      delay(200);
+      delay(100);
       return;
     }
   }
@@ -375,7 +392,7 @@ void checkButValues() {
     mode = (mode + NB_MODE) % NB_MODE;
     // Refresh play screen
     pot_change = mode == MODE_PLAY;
-    delay(200);
+    delay(100);
   }
   
   if(SR.Button_Pin_Read(BUTTON_WHITE_RIGHT)) {
@@ -383,7 +400,7 @@ void checkButValues() {
     mode = mode % NB_MODE;
     // Refresh play screen
     pot_change = mode == MODE_PLAY;
-    delay(200);
+    delay(100);
   }
   
 }
@@ -409,52 +426,58 @@ void updateLcd() {
     }
     
     if(bitmap_mode) {
-      lcd.clear();
-      lcd.setCursor(0,1);
-      for(int i = 0; i <  track_step[bank][inst]; i++) {
-        lcd.setCursor(i % 16, i / 16);
-        if(i == count_step[bank][inst]) {
-          lcd.print((char) 255);
-        } else if(beat_holder[bank][inst][i]) {
-          lcd.print((char) 124);
-        } else {
-          lcd.print((char) 95);
+      if( (play_pattern && (bitmap_mode_step != count_step[bank][inst])) 
+      || (bitmap_mode_step == 0 && pot_change)) {
+        pot_change = false;
+        bitmap_mode_step = count_step[bank][inst];
+        lcd.clear();
+        lcd.setCursor(0,1);
+        for(i = 0; i <  track_step[bank][inst]; i++) {
+          lcd.setCursor(i % 16, i / 16);
+          if(i == count_step[bank][inst] && play_pattern) {
+            lcd.print((char) 255);
+          } else if(beat_holder[bank][inst][i]) {
+            lcd.print((char) 124);
+          } else {
+            lcd.print((char) 95);
+          }
         }
       }
-      delay(100);
     } else {
       if(pot_change || old_lcd_mode_play != lcd_mode_play) {
         lcd.clear();
         pot_change = false;
         old_lcd_mode_play = lcd_mode_play;
-      }
-      if(lcd_mode_play == LCD_MODE_PLAY_01) {
-        lcd.setCursor(0,0);
-        lcd.print("NOT SWG RYT");
-        lcd.setCursor(0, 1);
-        lcd.print(midi_note[bank][inst],DEC);
-        lcd.setCursor(4,1);
-        lcd.print(track_swing[bank][inst]);
-        lcd.setCursor(8,1);
-        if(rythm_mode[bank][inst] == RYTHM_MODE_POLY) 
-          lcd.print("POL ");
-        else
-          lcd.print("SOL ");
-      } else {
-        lcd.setCursor(0,0);
-        lcd.print("B   STP PLS OFF ");
-        lcd.setCursor(2,0);
-        lcd.print(bank,DEC);
-        lcd.setCursor(0,1);
-        lcd.print("I   ");
-        lcd.setCursor(2,1);
-        lcd.print(inst,DEC);
-        lcd.setCursor(4,1);
-        lcd.print(track_step[bank][inst],DEC);
-        lcd.setCursor(8,1);
-        lcd.print(track_puls[bank][inst],DEC);
-        lcd.setCursor(12,1);
-        lcd.print((int) track_offs[bank][inst],DEC);
+      
+        if(lcd_mode_play == LCD_MODE_PLAY_01) {
+          lcd.setCursor(0,0);
+          lcd.print("NOT SWG RYT");
+          lcd.setCursor(0, 1);
+          sprintf(midi_note_label, midi_note_labels[midi_note[bank][inst] % 12], midi_note[bank][inst] / 12);
+          lcd.print(midi_note_label);
+          lcd.setCursor(4,1);
+          lcd.print(track_swing[bank][inst]);
+          lcd.setCursor(8,1);
+          if(rythm_mode[bank][inst] == RYTHM_MODE_POLY) 
+            lcd.print("POL ");
+          else
+            lcd.print("SOL ");
+        } else {
+          lcd.setCursor(0,0);
+          lcd.print("B   STP PLS OFF ");
+          lcd.setCursor(2,0);
+          lcd.print(bank,DEC);
+          lcd.setCursor(0,1);
+          lcd.print("I   ");
+          lcd.setCursor(2,1);
+          lcd.print(inst,DEC);
+          lcd.setCursor(4,1);
+          lcd.print(track_step[bank][inst],DEC);
+          lcd.setCursor(8,1);
+          lcd.print(track_puls[bank][inst],DEC);
+          lcd.setCursor(12,1);
+          lcd.print((int) track_offs[bank][inst],DEC);
+        }
       }
     }
   }
@@ -545,8 +568,8 @@ void Count_PPQN() {
       byte data = Serial.read();
       if(data == MIDI_START) {
           play_pattern = 1;
-          for(int i = 0; i < NB_BANK; i++) {
-            for(int j = 0; j < 4; j++) {
+          for(i = 0; i < NB_BANK; i++) {
+            for(j = 0; j < 4; j++) {
               count_ppqn[i][j] = -1;
             }
           }
@@ -554,8 +577,9 @@ void Count_PPQN() {
       
       else if(data == MIDI_STOP ) {
         play_pattern = 0;
-        for(int i = 0; i < NB_BANK; i++) {
-          for(int j = 0; j < 4; j++) {
+        bitmap_mode_step = 0;
+        for(i = 0; i < NB_BANK; i++) {
+          for(j = 0; j < 4; j++) {
             count_step[i][j] = 0;
             count_ppqn[i][j] = -1;
           }
@@ -565,17 +589,19 @@ void Count_PPQN() {
         int time = millis();
         bpm = (time - old_clock_time) / 24 * 60000;
         old_clock_time = time;
-        for(int i = 0; i < NB_BANK; i++) {
-          for(int j = 0; j < 4; j++) {
+        for(i = 0; i < NB_BANK; i++) {
+          for(j = 0; j < 4; j++) {
             count_ppqn[i][j]++;
             int current_track_step = 0;
+          
             // Apply swing
-            if( count_ppqn[i][j] <= track_scale[i][j] )
-              current_track_step = track_swing[i][j] * (count_ppqn[i][j] / track_scale[i][j]); //stretch
-            else
-              current_track_step = (2-track_swing[i][j]) * (count_ppqn[i][j] / track_scale[i][j])
-                                 + (track_scale[i][j])   * (track_swing[i][j]-1); //compress 
-              
+            // Delay the second 16th note within each 8th note.
+            // ie. delay all the even-numbered 16th notes within the beat (2, 4, 6, 8, etc.)
+            current_track_step = count_ppqn[i][j] / track_scale[i][j]; //stretch
+            if(current_track_step % 2 == 0) {
+              current_track_step = (count_ppqn[i][j] - track_swing[i][j]) / track_scale[i][j];
+            }
+            
             count_step[i][j] = current_track_step;   
             if(count_ppqn[i][j] >= (track_step[i][j] * track_scale[i][j]) - 1) {
               count_ppqn[i][j] = -1;
@@ -591,24 +617,26 @@ void Count_PPQN() {
     timer_time=2500000/bpm;
     Serial.write(MIDI_CLOCK);
     if(!play_pattern) {
-      for(int i = 0; i < NB_BANK; i++) {
-        for(int j = 0; j < 4; j++) {
+      for(i = 0; i < NB_BANK; i++) {
+        for(j = 0; j < 4; j++) {
           count_ppqn[i][j] = -1;
           count_step[i][j] = 0;
         }
       }
     } else {   
-      for(int i = 0; i < NB_BANK; i++) {
-        for(int j = 0; j < 4; j++) {
+      for(i = 0; i < NB_BANK; i++) {
+        for(j = 0; j < 4; j++) {
           count_ppqn[i][j]++;
           int current_track_step = 0;
+          
           // Apply swing
-          if( count_ppqn[i][j] <= track_scale[i][j] )
-            current_track_step = track_swing[i][j] * (count_ppqn[i][j] / track_scale[i][j]); //stretch
-          else
-            current_track_step = (2-track_swing[i][j]) * (count_ppqn[i][j] / track_scale[i][j])
-                               + (track_scale[i][j])   * (track_swing[i][j]-1); //compress 
-              
+          // Delay the second 16th note within each 8th note.
+          // ie. delay all the even-numbered 16th notes within the beat (2, 4, 6, 8, etc.)
+          current_track_step = count_ppqn[i][j] / track_scale[i][j]; //stretch
+          if(current_track_step % 2 == 0) {
+            current_track_step = (count_ppqn[i][j] - track_swing[i][j]) / track_scale[i][j];
+          }
+          
           count_step[i][j] = current_track_step;   
           if(count_ppqn[i][j] >= (track_step[i][j] * track_scale[i][j]) - 1) {
             count_ppqn[i][j] = -1;
@@ -629,7 +657,7 @@ void euclid(int8_t bank, int8_t inst) {
     free(beat_holder[bank][inst]);
     if(track_step[bank][inst] > 0) {
       beat_holder[bank][inst] = (boolean *) malloc(track_step[bank][inst]);
-      for(int i = 0; i < track_step[bank][inst]; i++) {
+      for(i = 0; i < track_step[bank][inst]; i++) {
         beat_holder[bank][inst][i] = false;
       }
     } else  
@@ -665,16 +693,14 @@ void euclid(int8_t bank, int8_t inst) {
   void build(int8_t level, int8_t bank, int8_t inst) {
     int newPos = pos++;
     // Apply offset
-    if(track_offs[bank][inst] != 0)  {
-      newPos += track_offs[bank][inst];
-      newPos = newPos % track_step[bank][inst];
-    }
+    newPos += track_offs[bank][inst] + 1;
+    newPos = newPos % track_step[bank][inst];  
     
     if(level == -1) {
       beat_holder[bank][inst][newPos] = false;
     }else if(level == -2){
-      beat_holder[bank][inst][newPos] = true;
-    }else{ 
+      beat_holder[bank][inst][newPos] = true; 
+    }else{
       pos--;
       for(int8_t i=0; i < count[level]; i++)
 	build(level-1, bank, inst); 
